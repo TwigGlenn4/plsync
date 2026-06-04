d42633c558bd64d7d4ade87a0afc7c20f32e8da1
TwigGlenn4 <twigglenn4@gmail.com>
2026-06-04 12:43:57 -0500

    switch to config file from constants

--- plsync.py
+++ plsync.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Twig Griffin
 # Download a playlist from YouTube/YouTube Music, without redownloading existing tracks.
-# Detects existing tracks using the 'purl' metadata tag populated with the URL by yt-dlp by default
+# * Detects existing tracks using the 'purl' metadata tag populated with the URL by yt-dlp by default
 # Dependencies
 #   tinytag - read metadata on existing tracks
 #   yt-dlp - get playlist information and download tracks
@@ -10,32 +10,33 @@
 #   pip3 install tinytag yt-dlp
 #   apt install rsgain
 
+import json
 from os import listdir as os_listdir
 from os import path as path
 from os import sep as PATH_SEPARATOR
 from sys import argv as sys_argv
-from tinytag import TinyTag
+
 import yt_dlp
+from tinytag import TinyTag
 
-CONFIG_MUSIC_PATH = "/home/twig/Music/Tracks/"
-CONFIG_COOKIES_PATH = "cookies.txt"
-CONFIG_DEFAULT_PLAYLIST_URLS = [ "https://music.youtube.com/playlist?list=PL899RuEJchv4GsWD1onvazX51kF3naNJN",
-                                 "https://music.youtube.com/playlist?list=PL899RuEJchv6ShrUrjGMunZFUl9VYdVNI"]
-CONFIG_ASK_BEFORE_DOWNLOADING = True
-
-# validate paths
-MUSIC_PATH = path.abspath(CONFIG_MUSIC_PATH) + PATH_SEPARATOR
-COOKIES_PATH = path.abspath(CONFIG_COOKIES_PATH)
+CONFIG_PATH = "~/.config/plsync/config.json"
+DEFAULT_CONFIG = {
+  "ask_before_downloading": True,
+  "cookies_path": "~/.config/plsync/cookies.txt",
+  "music_path": "~/Music/Tracks",
+  "playlist_urls": [ "https://music.youtube.com/playlist?list=OLAK5uy_k8FKntOa4ITd-iDboWkQD-0z0Ju__9cEk" ],
+  "filename_format": "%(artist,channel|Unknown)s - %(track,title|Unknown)s.%(ext)s"
+}
 
 
 YDL_OPTS = {
-  'cookiefile': COOKIES_PATH,
+  'cookiefile': "",
   'extract_flat': 'discard_in_playlist',
   'final_ext': 'mp3',
   'format': 'bestaudio/best',
   'fragment_retries': 2,
   'ignoreerrors': 'only_download',
-  'outtmpl': {'default': MUSIC_PATH+'%(artist,channel|Unknown)s - %(track,title|Unknown)s.%(ext)s',
+  'outtmpl': {'default': '%(artist,channel|Unknown)s - %(track,title|Unknown)s.%(ext)s',
       'pl_thumbnail': ''},
   'postprocessors': [{'key': 'FFmpegExtractAudio',
       'nopostoverwrites': False,
@@ -58,7 +59,7 @@ YDL_OPTS = {
   'writethumbnail': True}
 
 YDL_DATA_OPTS = {
-  'cookiefile': COOKIES_PATH,
+  'cookiefile': "",
   'extract_flat': True,  # Extract only the video information, not the actual videos
   'force_generic_extractor': True,
   'noplaylist': False,
@@ -66,6 +67,39 @@ YDL_DATA_OPTS = {
   'no_warnings': True, # Hide "YouTube Music is not directly supported" warning
   'simulate': True, # Don't actually download, just simulate to get info
 }
+
+
+def read_config_file(config_path: str) -> dict:
+  config_path = path.expanduser(config_path)
+
+  print("Using config file", config_path)
+  with open(config_path, "r") as config_file:
+    loaded_config = json.load(config_file)
+    config = DEFAULT_CONFIG
+    for key in config:
+      if key in loaded_config:
+        config[key] = loaded_config[key]
+        print(f"[CONFIG] read {key}: {config[key]}")
+
+  # expand paths
+  config["cookies_path"] = path.expanduser(config["cookies_path"])
+  config["music_path"] = path.expanduser(config["music_path"])
+
+  if str(config["music_path"])[-1] != PATH_SEPARATOR: # add path seperator to music_path
+    config["music_path"] = config["music_path"] + PATH_SEPARATOR
+
+  if not path.isfile(config["cookies_path"]):
+    print(f"[CONFIG] could not open cookie file \"{config["cookies_path"]}\"")
+    config["cookies_path"] = None
+
+  # update options
+  YDL_OPTS["cookiefile"] = config["cookies_path"]
+  YDL_DATA_OPTS["cookiefile"] = config["cookies_path"]
+
+  YDL_OPTS["outtmpl"]["default"] = config["music_path"] + config["filename_format"]
+
+  return config
+
 
 def get_youtube_slug( link:str ) -> str:
   splitStr = "v="
@@ -98,10 +132,10 @@ def find_local_songs( path:str ) -> list[str]:
     if slug == "":
       print(f"  Could not parse youtube slug, skipping {filename}")
       continue
-    
+
     # print("  [" + slug + "]: " + tag.artist + " - " + tag.title)
     # print()
-    
+
     found_songs.append(slug)
 
   return found_songs
@@ -112,7 +146,7 @@ def find_playlist_songs_ytdlp(playlist_link:str) -> list[str]:
 
   found_songs = []
   playlist_info = None
-  
+
   with yt_dlp.YoutubeDL(YDL_DATA_OPTS) as ytdl:
     playlist_info = ytdl.extract_info(playlist_link, download=False)
 
@@ -134,16 +168,16 @@ def find_playlist_songs_ytdlp(playlist_link:str) -> list[str]:
 
   else:
     print("  Invalid link!")
-    
+
   return found_songs
-    
+
 
 
 # Find the songs in remote_songs that are not in local_songs
 def get_songs_needed(local_songs:list[str], remote_songs:list[str]) -> list[str]:
   songs_needed = []
   for song in remote_songs:
-    if not song in local_songs:
+    if song not in local_songs:
       songs_needed.append(song)
 
   return songs_needed
@@ -161,7 +195,7 @@ def download_song(ytdl:yt_dlp.YoutubeDL, slug:str) -> str:
     error_slug = slug
     # with yt_dlp.YoutubeDL(YDL_DATA_OPTS) as ytdl:
     print(f"    ERROR: Error while downloading [{slug}]. NOTE: Can't tell if serious or non-fatal error. Song may have been downloaded fine.")
-    
+
   return error_slug
 
 
@@ -174,25 +208,25 @@ def deduplicate(list1:list, list2:list):
 
 def main():
 
-  print(COOKIES_PATH)
-  exit()
+  config = read_config_file(CONFIG_PATH)
+
 
   # Find all local songs
   print("Finding local songs...")
-  found_songs = find_local_songs(MUSIC_PATH)
+  found_songs = find_local_songs(config["music_path"])
   print(f"  Found {len(found_songs)} songs locally." )
 
 
-  # Use arguments as playlist urls if they exist, default to CONFIG_DEFAULT_PLAYLIST_URLS
+  # Use arguments as playlist urls if they exist, default to DEFAULT_CONFIG["playlist_urls"]
   # print(f"len(sys_argv)={len(sys_argv)}")
-  playlist_urls = CONFIG_DEFAULT_PLAYLIST_URLS
+  playlist_urls = config["playlist_urls"]
   if len(sys_argv) > 1:
     playlist_urls = []
     for i in range(1, len(sys_argv)):
       playlist_urls[i-1] = sys_argv[i]
-  
+
   if (playlist_urls == [] or not isinstance(playlist_urls[0], str)):
-    print("No URL specified! Please give a URL in quotes as an argument, or populate the CONFIG_DEFAULT_PLAYLIST_URLS constant in the script.")
+    print("No URL specified! Please give a URL in quotes as an argument, or populate the \"playlist_urls\" option in the config.")
     return 1
 
   # Find songs in the online playlists that don't exist locally
@@ -213,13 +247,13 @@ def main():
   # print(download_list)
   print(f"Need to download {len(download_list)} of {len(unique_pl_list)} songs.")
 
-  if CONFIG_ASK_BEFORE_DOWNLOADING:
+  if config["ask_before_downloading"]:
     download = input("Continue with Download? (y/N): ")
     if not (download == "y" or download == "Y"):
       return 0
-    
+
   errored_slugs = []
-  
+
   # Download the songs
   with yt_dlp.YoutubeDL(YDL_OPTS) as yt_downloader:
     num_songs_downloaded = 0
@@ -236,6 +270,7 @@ def main():
   if len(errored_slugs) > 0:
     print("Errored songs: " + str(errored_slugs))
   return 0
+
 
 if __name__ == "__main__":
   try:
