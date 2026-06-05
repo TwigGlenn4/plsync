4eeb180b7ea0b958b8b65955d4f3c2b73da8c870
TwigGlenn4 <twigglenn4@gmail.com>
2026-06-04 20:18:10 -0500

    support multiple folders each with a list of playlists

--- plsync.py
+++ plsync.py
@@ -11,21 +11,26 @@
 #   apt install rsgain
 
 import json
-from os import listdir as os_listdir
-from os import path as path
-from os import sep as PATH_SEPARATOR
+from os import listdir, makedirs, path
 from sys import argv as sys_argv
 
 import yt_dlp
 from tinytag import TinyTag
+from yt_dlp.utils import DownloadError, ExtractorError, PostProcessingError
 
 CONFIG_PATH = "~/.config/plsync/config.json"
 DEFAULT_CONFIG = {
   "ask_before_downloading": True,
   "cookies_path": "~/.config/plsync/cookies.txt",
-  "music_path": "~/Music/Tracks",
-  "playlist_urls": [ "https://music.youtube.com/playlist?list=OLAK5uy_k8FKntOa4ITd-iDboWkQD-0z0Ju__9cEk" ],
-  "filename_format": "%(artist,channel|Unknown)s - %(track,title|Unknown)s.%(ext)s"
+  "filename_format": "%(artist,channel|Unknown)s - %(track,title|Unknown)s.%(ext)s",
+  "ytdlp_quiet": False,
+  "playlists": [
+    {
+      "path": "~/Music/Tracks",
+      "urls": [ "https://music.youtube.com/playlist?list=OLAK5uy_k8FKntOa4ITd-iDboWkQD-0z0Ju__9cEk",
+                "https://music.youtube.com/playlist?list=OLAK5uy_nYgA6_SXlD09Kc6G0TmA91je3qDeD9lzs" ]
+    }
+  ]
 }
 
 
@@ -55,8 +60,9 @@ YDL_OPTS = {
       'when': 'after_move'}],
   'retries': 1,
   'warn_when_outdated': True,
-  # 'quiet': True,
-  'writethumbnail': True}
+  'quiet': True,
+  'writethumbnail': True
+}
 
 YDL_DATA_OPTS = {
   'cookiefile': "",
@@ -65,12 +71,18 @@ YDL_DATA_OPTS = {
   'noplaylist': False,
   'quiet': True,  # Suppress console output
   'no_warnings': True, # Hide "YouTube Music is not directly supported" warning
-  'simulate': True, # Don't actually download, just simulate to get info
+  'simulate': True # Don't actually download, just simulate to get info
 }
 
 
+def expand_path(work_path:str) -> str:
+  """Expand environment vars and the '~' home shortcut"""
+  return path.expanduser(path.expandvars(work_path))
+
+
 def read_config_file(config_path: str) -> dict:
-  config_path = path.expanduser(config_path)
+  # expand vars ($HOME) and user (~)
+  config_path = expand_path(config_path)
 
   print("Using config file", config_path)
   with open(config_path, "r") as config_file:
@@ -80,22 +92,30 @@ def read_config_file(config_path: str) -> dict:
       if key in loaded_config:
         config[key] = loaded_config[key]
 
-  # expand paths
-  config["cookies_path"] = path.expanduser(config["cookies_path"])
-  config["music_path"] = path.expanduser(config["music_path"])
-
-  if str(config["music_path"])[-1] != PATH_SEPARATOR: # add path seperator to music_path
-    config["music_path"] = config["music_path"] + PATH_SEPARATOR
-
+  # expand and sanitize cookies_path
+  config["cookies_path"] = expand_path(config["cookies_path"])
   if not path.isfile(config["cookies_path"]):
     print(f"[CONFIG] could not open cookie file \"{config["cookies_path"]}\"")
     config["cookies_path"] = None
 
-  # update options
+  # store yt-dlp cookie file
   YDL_OPTS["cookiefile"] = config["cookies_path"]
   YDL_DATA_OPTS["cookiefile"] = config["cookies_path"]
 
-  YDL_OPTS["outtmpl"]["default"] = config["music_path"] + config["filename_format"]
+  YDL_OPTS["quiet"] = config["ytdlp_quiet"]
+  if config["ytdlp_quiet"]: # if ytdlp_quiet, also make rsgain quiet
+    for post_processor in YDL_OPTS["postprocessors"]:
+      if "exec_cmd" in post_processor:
+        if post_processor["exec_cmd"][0][:6] == "rsgain":
+          post_processor["exec_cmd"][0] += " -q"
+
+  # verify playlist list
+  for playlist in config["playlists"]:
+    if "path" not in playlist or "urls" not in playlist: # if playlist is malformed, skip it
+      config["playlists"].remove(playlist)
+      print("[CONFIG] malformed playlist (missing \"path\" or \"urls\" keys):", playlist)
+    else:
+      playlist["path"] = expand_path(playlist["path"])
 
   return config
 
@@ -108,12 +128,13 @@ def get_youtube_slug( link:str ) -> str:
   return slug
 
 
-def find_local_tracks( path:str ) -> list[str]:
+def find_local_tracks( folder: str ) -> list[str]:
   found_songs = []
 
   # print(os_listdir(path))
-  for fname in os_listdir(path):
-    filename = path + fname
+
+  for fname in listdir(folder):
+    filename = path.join(folder, fname)
     # print(filename)
 
     if not TinyTag.is_supported(filename):
@@ -141,32 +162,37 @@ def find_local_tracks( path:str ) -> list[str]:
 
 
 def find_playlist_songs_ytdlp(playlist_link:str) -> list[str]:
-  print("  Finding playlist using yt-dlp...")
+  print("  Finding playlist using yt-dlp...    ", end="", flush=True)
 
-  found_songs = []
+  found_songs: list[str] = []
   playlist_info = None
 
   with yt_dlp.YoutubeDL(YDL_DATA_OPTS) as ytdl:
-    playlist_info = ytdl.extract_info(playlist_link, download=False)
+    try:
+      playlist_info = ytdl.extract_info(playlist_link, download=False)
+    except (DownloadError, ExtractorError):
+      # ytdl.extract_info() already printed an error message
+      return []
+
 
   # print(playlist_info['_type'])
   # print(playlist_info)
 
   if "_type" in playlist_info and playlist_info["_type"] == "playlist":
-    print(f"    Found playlist \"{playlist_info['title']}\" containing {playlist_info['playlist_count']} songs")
+    print(f"found playlist \"{playlist_info['title']}\" containing {playlist_info['playlist_count']} songs")
     for song in playlist_info['entries']:
       slug = song['id']
       found_songs.append(slug)
       # print("    "+slug)
 
   elif "duration_string" in playlist_info:
-    print(f"    Found video \"{playlist_info['title']}\" by {playlist_info['channel']}")
+    print(f"found video \"{playlist_info['title']}\" by {playlist_info['channel']}")
     slug = playlist_info['id']
     found_songs = [slug]
     # print("    "+slug)
 
   else:
-    print("  Invalid link!")
+    print("Invalid link!")
 
   return found_songs
 
@@ -185,17 +211,17 @@ def get_songs_needed(local_songs:list[str], remote_songs:list[str]) -> list[str]
 
 def download_song(ytdl:yt_dlp.YoutubeDL, slug:str) -> str:
   # print(f"Downloading {slug}...")
-  error_code = 1
-  error_slug = ""
-  # try:
-  error_code = ytdl.download(slug)
+  error = ""
+  try:
+    track_info = ytdl.extract_info(slug, download=False)
+    print(f"{track_info["artists"][0]} - {track_info["title"]}")
+    ytdl.process_ie_result(track_info, download=True)
 
-  if error_code != 0:
-    error_slug = slug
-    # with yt_dlp.YoutubeDL(YDL_DATA_OPTS) as ytdl:
-    print(f"    ERROR: Error while downloading [{slug}]. NOTE: Can't tell if serious or non-fatal error. Song may have been downloaded fine.")
+  except (DownloadError, ExtractorError, PostProcessingError) as e:
+    error = slug
+    print(f"Error while downloading [{slug}]: {e}")
 
-  return error_slug
+  return error
 
 
 def deduplicate(list1:list, list2:list):
@@ -205,8 +231,10 @@ def deduplicate(list1:list, list2:list):
   return list1
 
 
-def download_playlist(download_path: str, playlist_urls: list[str], ask_before_downloading: bool) -> int:
+def download_playlist(download_path: str, playlist_urls: list[str], config: dict) -> int:
   print("Processing", download_path)
+
+  makedirs(download_path, exist_ok=True) # ensure download path exists
 
   local_tracks = find_local_tracks(download_path)
   print(f"  Found {len(local_tracks)} songs locally." )
@@ -235,28 +263,34 @@ def download_playlist(download_path: str, playlist_urls: list[str], ask_before_d
   print(f"  Need to download {len(download_list)} of {len(unique_playlist_tracks)} songs.")
 
 
-  if ask_before_downloading:
-    download = input("  Continue with Download? (y/N): ").strip()
+  if config["ask_before_downloading"]:
+    download = input("  Continue with download? (y/N): ").strip()
     if not (download.lower() == "y" or download.lower() == "yes"):
       return 0
 
   errored_track_slugs = []
 
+  # setup yt-dlp options with path.
+
+  yt_dlp_opts = YDL_OPTS.copy()
+  yt_dlp_opts["outtmpl"]["default"] = path.join(download_path, config["filename_format"])
+
   # Download the songs
-  with yt_dlp.YoutubeDL(YDL_OPTS) as yt_downloader:
+  with yt_dlp.YoutubeDL(yt_dlp_opts) as yt_downloader:
     num_songs_downloaded = 0
     for slug in download_list:
       num_songs_downloaded += 1
-      print(f"    Downloading song {num_songs_downloaded}/{len(download_list)}")
+      print(f"    Downloading song {num_songs_downloaded}/{len(download_list)}: ", end="", flush=True)
       error_slug = download_song(yt_downloader, slug)
       if error_slug != "":
         errored_track_slugs.append(error_slug)
-      print("--------------------")
+      if not config["ytdlp_quiet"]:
+        print("----------------------------------------")
 
-  print(f"  Done, downloaded {len(download_list)} songs!")
+  print(f"  Downloaded {len(download_list)} songs!")
 
   if len(errored_track_slugs) > 0:
-    print(f"  Errored songs: ({len(errored_track_slugs)}) {str(errored_track_slugs)}")
+    print(f"  Errored tracks: ({len(errored_track_slugs)}) {str(errored_track_slugs)}")
 
   return len(download_list)
 
@@ -267,7 +301,8 @@ def main():
 
   config = read_config_file(CONFIG_PATH)
 
-  download_playlist(config["music_path"], config["playlist_urls"], config["ask_before_downloading"])
+  for playlist in config["playlists"]:
+    download_playlist(playlist["path"], playlist["urls"], config)
 
   return 0
 
